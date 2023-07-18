4efc9d95d33ff92aedb791bf022d779803a9e772
Maksim Kauger <136794025+ITactics@users.noreply.github.com>
2023-07-18 14:56:11 +0300

    Update backup_script_template.sh

--- my-ansible/backup_project/backup_script_template.sh
+++ my-ansible/backup_project/backup_script_template.sh
@@ -18,10 +18,13 @@ do
     # Проверка кода завершения mysqldump
     if [ $? -eq 0 ]; then
         echo "Резервное копирование базы данных $database_name завершено."
+ 
+        # Копирование резервной копии в Google Cloud Storage
+        gsutil cp $backup_dir/$database_name.sql gs://my-bucket-name/
     else
         echo "Ошибка при резервном копировании базы данных $database_name."
     fi
 done
-
+ 
 # Удаление резервных копий старше 30 дней
 find $backup_dir -type f -name "*.sql" -mtime +30 -exec rm -f {} \;
